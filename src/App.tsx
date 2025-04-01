a4ece30711ee6294ba73ade85aa05aaf14e76b40
Mylena Marques Ribeiro <ribeiro.mylena1@gmail.com>
2025-04-01 12:23:53 -0300

    Criando uma API fake com json-server e um seletor assíncrono

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,30 @@
-// import { useState } from 'react';
 import style from './App.module.scss';
 import Card from './components/Card';
 import Formulario from './components/Formulario';
-// import { IEvento } from './interfaces/IEvento';
 import Calendario from './components/Calendario';
 import ListaDeEventos from './components/ListaDeEventos';
 import { RecoilRoot } from 'recoil';
+import { Suspense } from 'react';
 
 function App() {
-
-  // const [filtro, setFiltro] = useState<Date | null>();
-
-  // const adicionarEvento = (evento: IEvento) => {
-  //   evento.id = Math.round((new Date()).getTime() / 1000);
-  // }
-
-  // const aplicarFiltro = (data: Date | null) => {
-  //   setFiltro(data);
-  // };
-
   return (
     <RecoilRoot>
-      <div className={style.App}>
-        <div className={style.Coluna}>
-          <Card>
-            <Formulario />
-          </Card>
-          <hr />
-          <Card>
-            {/* <ListaDeEventos aoFiltroAplicado={aplicarFiltro} /> */}
-            <ListaDeEventos />
-          </Card>
+      <Suspense fallback="Está carregando">
+        <div className={style.App}>
+          <div className={style.Coluna}>
+            <Card>
+              <Formulario />
+            </Card>
+            <hr />
+            <Card>
+              <ListaDeEventos />
+            </Card>
+          </div>
+          <div className={style.Coluna}>
+            <Calendario />
+          </div>
         </div>
-        <div className={style.Coluna}>
-          <Calendario />
-        </div>
-      </div>
+      </Suspense>
     </RecoilRoot>
   );
 }
--- src/state/seletores/index.ts
+++ src/state/seletores/index.ts
@@ -1,5 +1,6 @@
 import { selector } from "recoil";
-import { filtroDeEventos, listaDeEventosState } from "../../atom";
+import { filtroDeEventos, listaDeEventosState } from "../atom";
+import { IEvento } from "../../interfaces/IEvento";
 
 export const eventosFiltradosState = selector({
     key: "eventosFiltradosState",
@@ -18,3 +19,16 @@ export const eventosFiltradosState = selector({
     }
 });
 
+export const eventosAsync = selector({
+    key: 'eventosAsync',
+    get: async () => {
+            const respostaHttp = await fetch('http://localhost:8080/eventos');
+            const eventosJson: IEvento[] = await respostaHttp.json();
+            return eventosJson.map(evento => ({
+                ...evento,
+                inicio: new Date(evento.inicio),
+                fim: new Date(evento.fim)
+            }));   
+    }
+
+})
